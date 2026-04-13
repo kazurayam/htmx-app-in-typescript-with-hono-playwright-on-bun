03e4ae76c8707d349ac30427e537096106d0cd92
kazurayam <kazuaki.urayama@gmail.com>
2026-04-13 18:10:23 +0900

    #5 bun run testmain -- performs E2E test for http://localhost:3001 using Playwright

---
+++ my-app/src/main.e2e.ts
@@ -0,0 +1,28 @@
+// src/main.e2e.ts
+import { describe, it, expect, beforeAll, afterAll, beforeEach, afterEach } from 'bun:test';
+import { Browser, chromium } from 'playwright-chromium';
+
+describe('Assert the server is up and running', async () => {
+    // Here I assume that the server at http://localhost:3001 is already up and running.
+    let browser: Browser;
+    beforeAll(async () => {
+        // launch the browser
+        browser = await chromium.launch()
+    })
+    it("Navigate to the top page, click the link Section 3; then <h1>Section3</h1> should be there", async () => {
+        // Create a new page and navigate to a URL
+        const page = await browser.newPage();
+        await page.goto('http://localhost:3001');
+        // Select the link
+        const link = page.getByText('Section 3');
+        expect(await link.isVisible()).toBeTrue();
+        // Click the link!
+        await link.click();
+        const h1 = page.getByText('Section3');
+        expect(await h1.isVisible()).toBeTrue();
+    });
+    afterAll(async () => {
+        // Clean up
+        await browser.close()
+    })
+})
