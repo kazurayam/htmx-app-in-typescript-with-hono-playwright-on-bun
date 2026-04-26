afe680c979e0a812076b670248c1a218d446be07
kazurayam <kazuaki.urayama@gmail.com>
2026-04-26 20:40:46 +0900

    done the section13

--- my-app/e2e/section13.test.ts
+++ my-app/e2e/section13.test.ts
@@ -21,6 +21,44 @@ describe('test http://localhost:3001/section13', async () => {
         await button.click()
     })
 
+    it("hx-boost(有効時) リンクの例", async () => {
+        // I don't see what's the use
+    })
+
+    it("hx-boost(無効時) リンクの例", async () => {
+        // I don't see what's the use
+    })
+
+    it("hx-boost(有効時) formの例", async () => {
+        // I don't see what's the use
+    })
+
+    it("hx-push-url(true)", async () => {
+        // I don't see what's the use
+    })
+
+    it("hx-push-url(false)", async () => {
+        // I don't see what's the use
+    })
+
+    it("hx-push-url(カスタムURL)", async () => {
+        // I don't see what's the use
+    })
+
+    it("hx-on", async () => {
+        // I don't see what's the use
+    })
+
+    it("htmx:before-request", async () => {
+        // I don't see what's the use
+    })
+
+
+    it("htmx:after-request", async () => {
+        // I don't see what's the use
+    })
+
+
     afterAll(async () => {
         await browser.close();
     });
