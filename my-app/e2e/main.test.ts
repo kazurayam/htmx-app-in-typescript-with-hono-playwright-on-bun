a868c4b7f855ce7b58c2c1079d3a6c2eccafba86
kazurayam <kazuaki.urayama@gmail.com>
2026-05-02 22:29:35 +0900

    #16 introduce the Playwright's expect(Locator).toHaveText(text)

--- my-app/e2e/main.test.ts
+++ my-app/e2e/main.test.ts
@@ -1,29 +1,41 @@
 // e2e/main.test.ts
 import { describe, it, expect, beforeAll, afterAll, beforeEach, afterEach } from 'bun:test';
-import { Browser, Page, chromium } from 'playwright-chromium';
+import * as PW from '@playwright/test';
 
 describe('Test if the server is up and running', async () => {
     // Here I assume that the server at http://localhost:3001 is already up and running.
-    let browser: Browser;
-    let page: Page;
+    let browser: PW.Browser;
+    let context: PW.BrowserContext;
+    let page: PW.Page;
     beforeAll(async () => {
         // launch the browser
-        browser = await chromium.launch({ headless: true, timeout: 10000 })
-        // Create a new page and navigate to a URL
-        page = await browser.newPage();
-        await page.goto('http://localhost:3001', { timeout: 10000 });
+        browser = await PW.chromium.launch({ headless: true })
     })
-    it("Navigate to the top page, click the link Section 3; then <h1>Section3</h1> should be there", async () => {
+    beforeEach(async () => {
+        // Create a new page object and navigate to the Top page
+        context = await browser.newContext();
+        page = await context.newPage();
+        await page.goto('http://localhost:3001');
+    })
+
+    it("In the Top page, click the link 'Section 3'; then will navigate to another URL where <h1>Section3</h1> is visible", async () => {
         // Select the link
-        const link = page.getByText('Section 3');
-        expect(await link.isVisible()).toBeTruthy();
+        const link: PW.Locator = page.getByText('Section 3');
+        await PW.expect(link).toBeVisible();
         // Click the link!
-        await link.click({ timeout: 10000 });
-        const h1 = page.getByText('Section3');
-        expect(await h1.isVisible()).toBeTruthy();
+        await link.click();
+        // Wait for another URL to load
+        await page.waitForLoadState('networkidle');
+        // Check if the page contains the expected text
+        const h1: PW.Locator = page.locator('css=h1')
+        await PW.expect(h1).toHaveText('Section3');
     });
+
+    afterEach(async () => {
+        await page.close();
+        await context.close();
+    })
     afterAll(async () => {
-        // Clean up
-        await browser.close()
+        await browser.close();
     })
 })
