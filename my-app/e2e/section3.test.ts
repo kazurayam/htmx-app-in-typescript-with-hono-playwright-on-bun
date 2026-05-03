e42b4719801c2fc0d900118febeaeb82a3029897
kazurayam <kazuaki.urayama@gmail.com>
2026-05-03 21:02:24 +0900

    #16 done the section3.test.ts

--- my-app/e2e/section3.test.ts
+++ my-app/e2e/section3.test.ts
@@ -1,68 +1,76 @@
 // e2e/section3.test.ts
 import { describe, it, expect, beforeAll, afterAll, beforeEach, afterEach } from 'bun:test';
-import { Browser, Page, chromium } from 'playwright-chromium';
+import * as PW from '@playwright/test';
 
 describe('test http://localhost:3001/section3', async () => {
     // Here I assume that the server at http://localhost:3001 is already up and running.
-    let browser: Browser;
-    let page: Page;
+    let browser: PW.Browser;
+    let page: PW.Page;
     beforeAll(async () => {
         // launch the browser
-        browser = await chromium.launch({ timeout: 10000 })
+        browser = await PW.chromium.launch()
+    })
+    beforeEach(async () => {
         // Create a new page and navigate to a URL
         page = await browser.newPage();
-        await page.goto('http://localhost:3001/section3', { timeout: 10000 });
+        // Navigate to the URL before each test
+        await page.goto('http://localhost:3001/section3');
     })
+
     it("click <button hx-get=/hello>, then the button should show GETリクエスト!", async () => {
         // Select the button
-        const button = page.locator('css=button[hx-get]');
-        expect(await button.isVisible()).toBeTruthy();
+        const button: PW.Locator = page.locator('css=button[hx-get]');
+        await PW.expect(button).toBeVisible();
         // Click the button!
         await button.click();
-        await page.waitForTimeout(500)
-        const span = page.getByText('GETリクエスト!');
-        expect(await span.isVisible()).toBeTruthy();
+        const span: PW.Locator = page.getByText('GETリクエスト!');
+        await PW.expect(span).toBeVisible();
     });
+
     it("click <button hx-post=/hello>, then the button should show POSTリクエスト!", async () => {
         // Select the button
-        const button = page.locator('css=button[hx-post]');
-        expect(await button.isVisible()).toBeTruthy();
+        const button: PW.Locator = page.locator('css=button[hx-post]');
+        await PW.expect(button).toBeVisible();
         // Click the button!
         await button.click();
-        await page.waitForTimeout(500)
-        const span = page.getByText('POSTリクエスト!');
-        expect(await span.isVisible()).toBeTruthy();
+        const span: PW.Locator = page.getByText('POSTリクエスト!');
+        await PW.expect(span).toBeVisible();
     });
+
     it("click <button hx-put=/hello>, then the button should show PUTリクエスト!", async () => {
         // Select the button
-        const button = page.locator('css=button[hx-put]');
-        expect(await button.isVisible()).toBeTruthy();
+        const button: PW.Locator = page.locator('css=button[hx-put]');
+        await PW.expect(button).toBeVisible();
         // Click the button!
         await button.click();
-        await page.waitForTimeout(500)
-        const span = page.getByText('PUTリクエスト!');
-        expect(await span.isVisible()).toBeTruthy();
+        const span: PW.Locator = page.getByText('PUTリクエスト!');
+        await PW.expect(span).toBeVisible();
     });
+
     it("click <button hx-patch=/hello>, then the button should show PATCHリクエスト!", async () => {
         // Select the button
-        const button = page.locator('css=button[hx-patch]');
-        expect(await button.isVisible()).toBeTruthy();
+        const button: PW.Locator = page.locator('css=button[hx-patch]');
+        await PW.expect(button).toBeVisible();
         // Click the button!
         await button.click();
-        await page.waitForTimeout(500)
-        const span = page.getByText('PATCHリクエスト!');
-        expect(await span.isVisible()).toBeTruthy();
+        const span: PW.Locator = page.getByText('PATCHリクエスト!');
+        await PW.expect(span).toBeVisible();
     });
+
     it("click <button hx-delete=/hello>, then the button should show DELETEリクエスト!", async () => {
         // Select the button
-        const button = page.locator('css=button[hx-delete]');
-        expect(await button.isVisible()).toBeTruthy();
+        const button: PW.Locator = page.locator('css=button[hx-delete]');
+        await PW.expect(button).toBeVisible();
         // Click the button!
         await button.click();
-        await page.waitForTimeout(500)
-        const span = page.getByText('DELETEリクエスト!');
-        expect(await span.isVisible()).toBeTruthy();
+        const span: PW.Locator = page.getByText('DELETEリクエスト!');
+        await PW.expect(span).toBeVisible();
     });
+
+    afterEach(async () => {
+        // Clean up
+        await page.close()
+    })
     afterAll(async () => {
         // Clean up
         await browser.close()
