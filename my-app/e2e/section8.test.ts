f9e73aa787d466f7a4f19168fdcc32044de35124
kazurayam <kazuaki.urayama@gmail.com>
2026-05-05 21:09:49 +0900

    #16 done the e2e/section8.test.ts

--- my-app/e2e/section8.test.ts
+++ my-app/e2e/section8.test.ts
@@ -1,12 +1,14 @@
 // e2e/section8.test.ts
 import { describe, it, expect, beforeAll, afterAll, beforeEach, afterEach } from 'bun:test';
-import { Browser, Page, chromium } from 'playwright-chromium';
+import * as PW from '@playwright/test';
 
 describe('test http://localhost:3001/section8', async () => {
-    let browser: Browser;
-    let page: Page;
+    let browser: PW.Browser;
+    let page: PW.Page;
     beforeAll(async () => {
-        browser = await chromium.launch({ timeout: 10000 });
+        browser = await PW.chromium.launch({ timeout: 10000 });
+    });
+    beforeEach(async () => {
         page = await browser.newPage();
         await page.goto('http://localhost:3001/section8', { timeout: 10000 })
     });
@@ -14,17 +16,22 @@ describe('test http://localhost:3001/section8', async () => {
     it("hx-trigger=every 1s", async () => {
         const p_as_target = page.locator('css=p#every-target');
         const content1 = await p_as_target.innerText();
-        await page.waitForTimeout(1500)
-        const content2 = await p_as_target.innerText();
         expect(content1).toMatch(/foo/);
+        await page.waitForTimeout(1100)
+        const content2 = await p_as_target.innerText();
         expect(content2).toMatch(/[0-9]+/);
-        expect(content1).not.toEqual(content2)
+        expect(content2).not.toEqual(content1)
+        await page.waitForTimeout(1100)
+        const content3 = await p_as_target.innerText();
+        expect(content3).toMatch(/[0-9]+/);
+        expect(content3).not.toEqual(content2)
+
     })
 
     it("hx-trigger=load delay:3s", async () => {
         const p1 = page.locator('css=p[hx-trigger="load delay:3s"]')
         expect((await p1.innerText()).match(/hoge/))
-        await page.waitForTimeout(3000);
+        await page.waitForTimeout(3100);
         const p2 = page.locator('css=p[hx-trigger="load delay:3s"]')
         expect(await p2.innerText()).toMatch(/[0-9]+/);
     })
@@ -41,6 +48,9 @@ describe('test http://localhost:3001/section8', async () => {
         expect(content1).not.toEqual(content2)
     })
 
+    afterEach(async () => {
+        await page.close();
+    });
     afterAll(async () => {
         await browser.close();
     });
