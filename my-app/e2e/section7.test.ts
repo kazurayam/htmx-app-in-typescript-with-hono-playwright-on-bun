33dac59ad411375c04089a93f4cb45d89d976a83
kazurayam <kazuaki.urayama@gmail.com>
2026-05-05 09:39:34 +0900

    #16 done the e2e/section7.test.ts

--- my-app/e2e/section7.test.ts
+++ my-app/e2e/section7.test.ts
@@ -1,38 +1,43 @@
 // e2e/section7.test.ts
 import { describe, it, expect, beforeAll, afterAll, beforeEach, afterEach } from 'bun:test';
-import { Browser, Page, chromium } from 'playwright-chromium';
+import * as PW from '@playwright/test';
 
 describe('test http://localhost:3001/section7', async () => {
-    let browser: Browser;
-    let page: Page;
+    let browser: PW.Browser;
+    let page: PW.Page;
     beforeAll(async () => {
-        browser = await chromium.launch({ timeout: 10000 });
+        browser = await PW.chromium.launch({ timeout: 10000 });
+    })
+    beforeEach(async () => {
         page = await browser.newPage();
         await page.goto('http://localhost:3001/section7', { timeout: 10000 })
     });
 
     it("hx-trigger=load delay:3s", async () => {
         const p = page.locator('css=p[hx-trigger="load delay:3s"]');
-        expect(await p.innerText()).toMatch(/foo/);
-        await page.waitForTimeout(4000);
-        expect(await p.innerText()).toMatch(/[0-9]+/);
+        await PW.expect(p).toContainText(/foo/);
+        await page.waitForTimeout(3500);
+        await PW.expect(p).toContainText(/[0-9]+/);
     })
 
     it("hx-trigger=revealed delay:1s", async () => {
         const p_as_target = page.locator('css=p#target1');
-        expect(await p_as_target.innerText()).toMatch(/foo/);
+        await PW.expect(p_as_target).toContainText(/foo/);
         const p_as_trigger = page.locator('css=p[hx-trigger="revealed delay:1s"]');
         // scroll to the element
         await p_as_trigger.scrollIntoViewIfNeeded();
         // will show a digit after delay:1s
-        await page.waitForTimeout(2000);
-        expect(await p_as_target.innerText()).toMatch(/[0-9]+/);
+        await page.waitForTimeout(1500);
+        await PW.expect(p_as_target).toContainText(/[0-9]+/);
     })
 
     /*
      * tests for ht-trigger="interset" are omitted as are cumbersome
      */
 
+    afterEach(async () => {
+        await page.close();
+    });
     afterAll(async () => {
         await browser.close();
     });
