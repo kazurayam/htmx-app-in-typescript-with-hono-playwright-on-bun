8c2d20ad3e9a5ed9ec0550f8d02db3f9227c6b92
kazurayam <kazuaki.urayama@gmail.com>
2026-05-08 21:51:12 +0900

    #16 doen the e2e/section12.test.ts

--- my-app/e2e/section12.test.ts
+++ my-app/e2e/section12.test.ts
@@ -1,12 +1,14 @@
 // e2e/section12.test.ts
 import { describe, it, expect, beforeAll, afterAll, beforeEach, afterEach } from 'bun:test';
-import { Browser, Page, Locator, chromium } from 'playwright-chromium';
+import * as PW from '@playwright/test';
 
 describe('test http://localhost:3001/section12', async () => {
-    let browser: Browser;
-    let page: Page;
+    let browser: PW.Browser;
+    let page: PW.Page;
     beforeAll(async () => {
-        browser = await chromium.launch();
+        browser = await PW.chromium.launch();
+    });
+    beforeEach(async () => {
         page = await browser.newPage();
         await page.goto('http://localhost:3001/section12')
         await page.waitForLoadState('networkidle', { timeout: 10_000 });
@@ -14,17 +16,22 @@ describe('test http://localhost:3001/section12', async () => {
 
     it("hx-params=*", async () => {
         const form = page.locator('css=form[hx-params="*"]')
+        await PW.expect(form).toBeVisible()
         // send texts in the input fields
         await form.locator('css=input[name="title"]').fill('greeting')
         await form.locator('css=input[name="name"]').fill('kazurayam')
         await form.locator('css=input[name="age"]').fill('66')
         // click the button
         const button = form.locator('button')
+        await PW.expect(button).toBeVisible()
+        // hx-post="/send-form" will take longer longer than 1 second
+        const responsePromise: Promise<PW.Response> =
+            page.waitForResponse(/\/send-form/, { timeout: 10_000 });
         await button.click()
-        await page.waitForTimeout(1500)
+        await responsePromise;
         // assert the target to contain "title=aaa&name=bbb&age=66"
-        const text = await page.locator('css=p#all-target').innerText()
-        expect(text).toMatch(/title=greeting&name=kazurayam&age=66/)
+        await PW.expect(page.locator('css=p#all-target'))
+            .toContainText(/title=greeting&name=kazurayam&age=66/)
     })
 
     it("hx-params=none", async () => { /* omit */ })
@@ -37,11 +44,13 @@ describe('test http://localhost:3001/section12', async () => {
         await form.locator('css=input[name="age"]').fill('66')
         // click the button
         const button = form.locator('css=button')
-        await button.click()
-        await page.waitForTimeout(1500)
+        const responsePromise: Promise<PW.Response> =
+            page.waitForResponse(/\/send-form/, { timeout: 10_000 });
+        await button.click();
+        await responsePromise;
         // assert the target to contain "title=aaa&age=66"
-        const text = await page.locator('css=p#param-target').innerText()
-        expect(text).toMatch(/title=greeting&age=66/)
+        await PW.expect(page.locator('css=p#param-target'))
+            .toContainText(/title=greeting&age=66/)
     })
 
     it("hx-param=not param-list", async () => { /* omit */ })
@@ -53,8 +62,10 @@ describe('test http://localhost:3001/section12', async () => {
         input.fill("kazurayam")
         // click the button
         const button = div.locator('css=button')
+        const responsePromise: Promise<PW.Response> =
+            page.waitForResponse(/\/send-form/, { timeout: 10_000 });
         await button.click()
-        await page.waitForTimeout(1500)
+        await responsePromise;
         // assert the target <p> contains "name=kazurayam"
         const p = page.locator('p#include-target')
         expect(await p.innerText()).toMatch(/kazurayam/)
@@ -64,8 +75,10 @@ describe('test http://localhost:3001/section12', async () => {
         // click the button
         const h2 = page.locator('xpath=//h2[contains(text(),"hx-vals")]')
         const button = h2.locator('xpath=following-sibling::button[1]')
+        const responsePromise: Promise<PW.Response> =
+            page.waitForResponse(/\/greeting/, { timeout: 10_000 });
         await button.click()
-        await page.waitForTimeout(500)
+        await responsePromise
         // assert the target <p> contains "title=Hello&name=Taro"
         const p = page.locator('css=p#vals-target1')
         expect(await p.innerText()).toMatch(/title=Hello&name=Taro/)
@@ -74,16 +87,21 @@ describe('test http://localhost:3001/section12', async () => {
     it("hx-vals=js:{lastkey: event.key}", async () => {
         // into the input field, press keys 'L', 'O', 'V', 'E'
         const input = page.locator('css=div[hx-target="#vals-target2"] input')
+        const responsePromise: Promise<PW.Response> =
+            page.waitForResponse(/\/last-key/, { timeout: 10_000 });
         input.press("L")
         input.press("O")
         input.press("V")
         input.press("E")
-        await page.waitForTimeout(500)
+        await responsePromise
         // asser the target <p> contains "lastkey=E"
         const p = page.locator('css=p#vals-target2')
         expect(await p.innerText()).toMatch(/lastkey=E/)
     })
 
+    afterEach(async () => {
+        await page.close();
+    })
     afterAll(async () => {
         await browser.close();
     });
