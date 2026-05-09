96d3b90b49df164787a8679f71eaa7da08983f6b
kazurayam <kazuaki.urayama@gmail.com>
2026-05-09 23:31:02 +0900

    #16 done the e2e/section15.test.ts

--- my-app/e2e/section15.test.ts
+++ my-app/e2e/section15.test.ts
@@ -1,23 +1,15 @@
 // e2e/section15.test.ts
 import { describe, it, expect, beforeAll, afterAll, beforeEach, afterEach } from 'bun:test';
-import { Browser, Page, Locator, chromium, BrowserContext } from 'playwright-chromium';
+import * as PW from '@playwright/test';
 
 describe('test http://localhost:3001/section15', async () => {
-    let browser: Browser;
-    let context: BrowserContext;
-    let page: Page;
-    /*
-     * I use "beforeEach" instead of "beforeAll" because I want to start with
-     * a fresh browser and page for each test case, and I want to navigate to
-     * the page before each test case to ensure that I am testing
-     * the same initial state for each test case. Some extensions may modify
-     * the page state, so starting with a fresh page for each test case helps
-     * to avoid interference between test cases.
-     */
+    let browser: PW.Browser;
+    let page: PW.Page;
+    beforeAll(async () => {
+        browser = await PW.chromium.launch({ headless: true });
+    });
     beforeEach(async () => {
-        browser = await chromium.launch({ headless: false });
-        context = await browser.newContext();
-        page = await context.newPage();
+        page = await browser.newPage();
         await page.goto('http://localhost:3001/section15')
         await page.waitForLoadState('networkidle', { timeout: 10_000 });
     });
@@ -25,10 +17,14 @@ describe('test http://localhost:3001/section15', async () => {
     it("hx-ext=head-support hx-head=merge", async () => {
         // click the button with hx-get="/update-head"
         const button = page.locator('css=div[hx-ext="head-support"] > button[hx-get="/update-head"]')
+        await PW.expect(button).toBeVisible()
+        const responsePromise: Promise<PW.Response> =
+            page.waitForResponse(/\/update-head/, { timeout: 10000 });
         await button.click()
-        await page.waitForTimeout(500)
-        // the response contains a head element with hx-head="merge", which should merge the new head with the existing head
-        // assert that the background-color: yellow is applied
+        await responsePromise;
+        // the response contains a head element with hx-head="merge",
+        // which should merge the new head with the existing head elements.
+        // Assert that the background-color: yellow is applied
         const backgroundColor = await page.evaluate(() => {
             return window.getComputedStyle(document.body).backgroundColor;
         });
@@ -37,26 +33,32 @@ describe('test http://localhost:3001/section15', async () => {
 
 
     it("hx-ext=head-support hx-head=re-eval", async () => {
-        // Listen for the console log from the page
+        // Let's listen for the console log from the page
         page.on('console', msg => {
             expect(msg.type()).toBe('log');
             expect(msg.text()).toBe("foo.js is loaded");
         })
         // click the button with hx-get="/re-eval-head"
         const button = page.locator('css=div[hx-ext="head-support"] > button[hx-get="/re-eval-head"]')
+        await PW.expect(button).toBeVisible()
+        const responsePromise: Promise<PW.Response> =
+            page.waitForResponse(/\/re-eval-head/, { timeout: 10000 });
         await button.click()
-        await page.waitForTimeout(500)
+        await responsePromise;
         // by clicking the button, foo.js should be loaded and executed,
-        // which logs "foo.js is loaded" to the console
+        // which will log "foo.js is loaded" to the console
     })
 
-    /*
     it("hx-ext=head-support hx-head=append", async () => {
         // click the button with hx-get="/append-head"
         const button = page.locator('css=div[hx-ext="head-support"] > button[hx-get="/append-head"]')
-        await button.click()
-        await page.waitForTimeout(500)
-        // the response contains a head element with hx-head="append", which should append the new head to the existing head
+        await PW.expect(button).toBeVisible()
+        const responsePromise: Promise<PW.Response> =
+            page.waitForResponse(/\/append-head/, { timeout: 10000 });
+        await button.click();
+        await responsePromise;
+        // the response contains a head element with hx-head="append",
+        // which should append the new head to the existing head
         // assert that the background-color: yellow is applied
         const backgroundColor = await page.evaluate(() => {
             return window.getComputedStyle(document.body).backgroundColor;
@@ -66,55 +68,65 @@ describe('test http://localhost:3001/section15', async () => {
 
     it("hx-ext=preload preloadあり", async () => {
         const button = page.locator('css=p#preload-target2 + button[preload]')
-        //const button = page.locator('xpath=//p[@id="preload-target2"]/following-sibling::button[1]')
+        await PW.expect(button).toBeVisible();
         // click the button with preload attribute
+        const responsePromise: Promise<PW.Response> =
+            page.waitForResponse(/\/hello/, { timeout: 10000 });
         await button.click()
-        await page.waitForTimeout(500)
+        await responsePromise;
         // the target should be updated to "GETリクエスト!"
         const target = page.locator('css=#preload-target2')
-        const text = await target.textContent()
-        expect(text).toBe("GETリクエスト!")
+        await PW.expect(target).toContainText(/GETリクエスト!/)
     })
 
     it("hx-ext=response-targets hx-get=/success", async () => {
-        // click the button with hx-get="/success"
         const button = page.locator('css=button[hx-get="/success"]')
+        await PW.expect(button).toBeVisible();
+        // click the button with hx-get="/success"
+        const responsePromise: Promise<PW.Response> =
+            page.waitForResponse(/\/success/, { timeout: 10000 });
         await button.click()
-        await page.waitForTimeout(500)
+        await responsePromise;
         // the target with id "success" should be updated to "Success!"
-        const successTarget = page.locator('css=#success')
-        const successText = await successTarget.textContent()
-        expect(successText).toBe("Success!")
+        const divSuccess = page.locator('css=#success')
+        await PW.expect(divSuccess).toContainText(/Success!/)
     })
 
     it("hx-ext=response-targets hx-get=/not-found", async () => {
-        // click the button with hx-get="/not-found"
         const button = page.locator('css=button[hx-get="/not-found"]')
+        await PW.expect(button).toBeVisible();
+        // click the button with hx-get="/not-found"
+        const responsePromise: Promise<PW.Response> =
+            page.waitForResponse(/\/not-found/, { timeout: 10000 });
         await button.click()
-        await page.waitForTimeout(500)
+        await responsePromise;
         // the target with id "not-found" should be updated to "Not Found!"
-        const notFoundTarget = page.locator('css=#not-found')
-        const notFoundText = await notFoundTarget.textContent()
-        expect(notFoundText).toBe("Not Found!")
+        const divNotFound = page.locator('css=#not-found')
+        await PW.expect(divNotFound).toContainText(/Not Found!/)
     })
 
     it("hx-ext=response-targets hx-get=/server-error", async () => {
-        // click the button with hx-get="/server-error"
         const button = page.locator('css=button[hx-get="/server-error"]')
+        await PW.expect(button).toBeVisible();
+        // click the button with hx-get="/server-error"
+        const responsePromise: Promise<PW.Response> =
+            page.waitForResponse(/\/server-error/, { timeout: 10000 });
         await button.click()
-        await page.waitForTimeout(500)
+        await responsePromise;
         // the target with id "server-error" should be updated to "Server Error!"
-        const serverErrorTarget = page.locator('css=#server-error')
-        const serverErrorText = await serverErrorTarget.textContent()
-        expect(serverErrorText).toBe("Internal Server Error!")
+        const divServerError = page.locator('css=#server-error')
+        await PW.expect(divServerError).toContainText("Internal Server Error!")
     })
-*/
+
+    // TODO: test hx-ext=ajax-header
+    // TODO: test hx-ext=json-enc
+    // TODO: test hx-ext=debug
+    // TODO: test hx-ext=remove-/**
 
     afterEach(async () => {
-        console.log("afterEach: closing context")
-        await context.close();
-        console.log("afterEach: closing browser")
+        await page.close();
+    });
+    afterAll(async () => {
         await browser.close();
-        console.log("afterEach: done")
     });
 });
