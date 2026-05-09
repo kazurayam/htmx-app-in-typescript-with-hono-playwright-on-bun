374a745388e5dea363e6471c2473514b97ec464a
kazurayam <kazuaki.urayama@gmail.com>
2026-05-09 22:36:44 +0900

    #16 done the e2e/section14.test.ts

--- my-app/e2e/section14.test.ts
+++ my-app/e2e/section14.test.ts
@@ -1,12 +1,14 @@
 // e2e/section14.test.ts
 import { describe, it, expect, beforeAll, afterAll, beforeEach, afterEach } from 'bun:test';
-import { Browser, Page, Locator, chromium } from 'playwright-chromium';
+import * as PW from '@playwright/test';
 
 describe('test http://localhost:3001/section14', async () => {
-    let browser: Browser;
-    let page: Page;
+    let browser: PW.Browser;
+    let page: PW.Page;
     beforeAll(async () => {
-        browser = await chromium.launch({ headless: true });
+        browser = await PW.chromium.launch({ headless: true });
+    });
+    beforeEach(async () => {
         page = await browser.newPage();
         await page.goto('http://localhost:3001/section14')
         await page.waitForLoadState('networkidle', { timeout: 10_000 });
@@ -16,25 +18,30 @@ describe('test http://localhost:3001/section14', async () => {
         const div = page.locator('css=div[hx-target="#target1"]')
         const button1 = div.locator('css=button:nth-child(1)')
         const button2 = div.locator('css=button:nth-child(2)')
-        const button3 = page.locator('xpath=//h2[contains(text(),"hx-targetの例")]/following-sibling::button[1]')
-        //
+        const button3 = div.locator('xpath=following-sibling::button[1]')
+        await PW.expect(button1).toBeVisible()
+        await PW.expect(button2).toBeVisible()
+        await PW.expect(button3).toBeVisible()
+        // ボタン1をクリックするとp#target1の内容が変化する。なぜならbutton1はdivの子要素で、divにhx-target="#target1"が指定されているから。
+        const responsePromise1: Promise<PW.Response> = page.waitForResponse(/\/random/, { timeout: 10000 });
         await button1.click()
-        await page.waitForTimeout(500)
-        expect(await page.locator('css=p#target1').innerText()).toMatch(/[0-9]+/)
-        //
+        await responsePromise1;
+        await PW.expect(page.locator('css=p#target1')).toContainText(/[0-9]+/)
+        // ボタン2をクリックするとp#target2の内容が変化する。なぜならbutton2にhx-target="#target2"が指定されているから。親要素のdivのhx-targetは継承されない。
+        const responsePromise2: Promise<PW.Response> = page.waitForResponse(/\/random/, { timeout: 10000 });
         await button2.click()
-        await page.waitForTimeout(500)
-        expect(await page.locator('css=p#target2').innerText()).toMatch(/[0-9]+/)
-        //
+        await responsePromise2;
+        await PW.expect(page.locator('css=p#target2')).toContainText(/[0-9]+/)
+        // ボタン3をクリックするとbutton3の内容が変化する。なぜならbutton3はdivの子要素ではないから。buttonにhx-targetが指定されていないから、デフォルト動作としてbutton自身の内容文字列が変化する。
+        const responsePromise3: Promise<PW.Response> = page.waitForResponse(/\/random/, { timeout: 10000 });
         await button3.click()
-        await page.waitForTimeout(500)
-        expect(await button3.innerText()).toMatch(/[0-9]+/)
+        await responsePromise3;
+        await PW.expect(button3).toContainText(/[0-9]+/)
     })
 
     it("継承 hx-confirmの例", async () => {
         const div = page.locator('xpath=//h2[contains(text(),"hx-confirmの例")]/following-sibling::div[1]')
-        //ボタンAとボタンBはクリックするとdialogを表示する
-        //というのも親要素のdivが指定したhx-conformを継承するから
+        await PW.expect(div).toBeVisible();
         let buttonName = ""
         page.on('dialog', async (dialog) => {
             await page.waitForTimeout(100)
@@ -42,34 +49,43 @@ describe('test http://localhost:3001/section14', async () => {
             expect(buttonName).toBeOneOf(['A','B'])
             dialog.accept()
         })
-        const buttonA = div.locator('xpath=button[1]')
+        //ボタンAとボタンBはクリックするとdialogを表示する
+        //というのも親要素のdivが指定したhx-conformを継承するから
+        const buttonA: PW.Locator = div.locator('xpath=button[1]')
         buttonName = 'A'
+        const responsePromiseA: Promise<PW.Response> = page.waitForResponse(/\/random/, { timeout: 10000 });
         await buttonA.click()
-        await page.waitForTimeout(100)
-        expect(await buttonA.innerText()).toMatch(/[0-9]+/)
+        await responsePromiseA
+        await PW.expect(buttonA).toContainText(/[0-9]+/)
         //
-        const buttonB = div.locator('xpath=button[2]')
+        const buttonB: PW.Locator = div.locator('xpath=button[2]')
         buttonName = 'B'
+        const responsePromiseB: Promise<PW.Response> = page.waitForResponse(/\/random/, { timeout: 10000 });
         await buttonB.click()
-        await page.waitForTimeout(100)
-        expect(await buttonB.innerText()).toMatch(/[0-9]+/)
+        await responsePromiseB;
+        await PW.expect(buttonB).toContainText(/[0-9]+/)
         //しかしボタンCはdialogを表示しない。hx-confirm="unset"しているから。
-        const buttonC = div.locator('xpath=button[3]')
+        const buttonC: PW.Locator = div.locator('xpath=button[3]')
         buttonName = 'C'
+        const responsePromiseC: Promise<PW.Response> = page.waitForResponse(/\/random/, { timeout: 10000 });
         await buttonC.click()
-        await page.waitForTimeout(100)
-        expect(await buttonC.innerText()).toMatch(/[0-9]+/)
+        await responsePromiseC;
+        await PW.expect(buttonC).toContainText(/[0-9]+/)
     })
 
     it("継承 hx-disinherit hx-target指定なし", async () => {
         //buttonの親要素のdivにhx-target属性が指定されている
         //buttonをクリックするとhx-targetに指定されたp要素の内容が変化する
         const h3 = page.locator('xpath=//h3[contains(text(),"hx-target指定なし")]')
+        await PW.expect(h3).toBeVisible()
         const button = h3.locator('xpath=following-sibling::div[1]/button')
+        await PW.expect(button).toBeVisible()
+        //buttonをクリックするとp要素の内容が変化する。なぜならbuttonはdivの子要素で、divにhx-target="#no-disinherit-target1"が指定されているから。
+        const responsePromise: Promise<PW.Response> = page.waitForResponse(/\/random/, { timeout: 10000 });
         await button.click()
-        await page.waitForTimeout(500)
+        await responsePromise;
         const p = page.locator('p#no-disinherit-target1')
-        expect(await p.innerText()).toMatch(/[0-9]+/)
+        await PW.expect(p).toContainText(/[0-9]+/)
     })
 
     it("継承 hx-disinherit hx-target指定あり", async () => {
@@ -77,12 +93,15 @@ describe('test http://localhost:3001/section14', async () => {
         //あわせてhx-disinherit="hx-target"と指定されているのでhx-targetが無効化される
         //buttonをクリックするとp要素は変化しないでbutton自身の内容が変化する
         const h3 = page.locator('xpath=//h3[contains(text(),"hx-targetを指定")]')
+        await PW.expect(h3).toBeVisible()
         const button = h3.locator('xpath=following-sibling::div[1]/button')
+        await PW.expect(button).toBeVisible()
+        const responsePromise: Promise<PW.Response> = page.waitForResponse(/\/random/, { timeout: 10000 });
         await button.click()
-        await page.waitForTimeout(500)
+        await responsePromise;
         const p = page.locator('p#disinherit-target1')
-        expect(await p.innerText()).toMatch(/foo/)
-        expect(await button.innerText()).toMatch(/[0-9]+/)
+        await PW.expect(p).toContainText(/foo/)
+        await PW.expect(button).toContainText(/[0-9]+/)
     })
 
     it("継承 hx-disinheritに*を指定", async () => {
@@ -90,15 +109,20 @@ describe('test http://localhost:3001/section14', async () => {
         //合わせてhx-disinherit="*"と指定されているのでhx-targetが無効化される
         //buttonをクリックするとp要素は変化しないでbutton自身の内容が変化する
         const h3 = page.locator('xpath=//h3[contains(text(),"*を指定")]')
+        await PW.expect(h3).toBeVisible()
         const button = h3.locator('xpath=following-sibling::div[1]/button')
+        await PW.expect(button).toBeVisible()
+        const responsePromise: Promise<PW.Response> = page.waitForResponse(/\/random/, { timeout: 10000 });
         await button.click()
-        await page.waitForTimeout(500)
+        await responsePromise;
         const p = page.locator('css=p#disinherit-target2')
-        expect(await p.innerText()).toMatch(/foo/)
-        expect(await button.innerText()).toMatch(/[0-9+]/)
+        await PW.expect(p).toContainText(/foo/)
+        await PW.expect(button).toContainText(/[0-9]+/)
     })
 
-
+    afterEach(async () => {
+        await page.close();
+    });
     afterAll(async () => {
         await browser.close();
     });
